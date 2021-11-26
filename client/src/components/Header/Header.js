119794236a5914ccedf6c9931e29b2557190d069
Dmytro Kozlovskyi <dmytrokozlovskyi@gmail.com>
2021-11-26 04:04:41 +0200

    Added dynamic Header title

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,13 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { PageHeader, Button } from "antd";
 import styles from "./Header.module.scss";
 
 import { AuthModalContext } from "../../hoc/AppContext";
+import { UserInfoContext } from "../../hoc/AppContext";
 
 function Header() {
   const { dispatch } = useContext(AuthModalContext);
+  const { userInfoState, userInfoDispatch } = useContext(UserInfoContext);
 
   const openModalHandeler = (modalType) =>
     dispatch({ type: "openModal", modalType: modalType });
@@ -14,16 +16,41 @@ function Header() {
     <PageHeader
       className={styles.header}
       onBack={() => null}
-      title="Title"
-      subTitle="This is a subtitle"
-      extra={[
-        <Button key="3" data-signin onClick={() => openModalHandeler("signin")}>
-          Login
-        </Button>,
-        <Button key="2" data-signup onClick={() => openModalHandeler("signup")}>
-          Registration
-        </Button>,
-      ]}
+      title={
+        userInfoState.isAuthenticated
+          ? `Welcome ${userInfoState.username}`
+          : "Please create account"
+      }
+      extra={
+        userInfoState.isAuthenticated ? (
+          <Button
+            key="3"
+            data-signin
+            onClick={() => {
+              userInfoDispatch({
+                type: "isUserLogged",
+                isAuthenticated: false,
+              });
+            }}>
+            Logout
+          </Button>
+        ) : (
+          [
+            <Button
+              key="3"
+              data-signin
+              onClick={() => openModalHandeler("signin")}>
+              Login
+            </Button>,
+            <Button
+              key="2"
+              data-signup
+              onClick={() => openModalHandeler("signup")}>
+              Registration
+            </Button>,
+          ]
+        )
+      }
     />
   );
 }
